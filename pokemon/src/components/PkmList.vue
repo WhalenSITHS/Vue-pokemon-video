2385b70b34a2560989b86717e8d47d7f081f538c
michael whalen <michael.whalen@sitechhs.com>
2021-01-04 13:27:53 -0500

    io;asdfhjik

---
+++ pokemon/src/components/PkmList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <h1>TEST</h1>
+    <ul class="pokemon-table">
+      <li
+        class="pokemon-list-item"
+        v-for="pokemon in pokemons"
+        :key="pokemon.name"
+      >
+        {{ pokemon.name }}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "PkmnList",
+  data() {
+    return { pokemons: [] };
+  },
+  props: ["id", "offset"],
+  mounted: function() {
+    this.fetchData();
+  },
+  methods: {
+    fetchData: async function() {
+      try {
+        const result = await fetch(
+          `https://pokeapi.co/api/v2/pokemon?limit=${this.id}&offset=${this.offset}`
+        );
+        const data = await result.json();
+        console.log(data);
+        this.pokemons = data.results;
+      } catch (error) {
+        alert(error);
+      }
+    },
+  },
+};
+</script>
+
+<style>
+h1 {
+  font-size: 5rem;
+  color: white;
+}
+li {
+  color: white;
+}
+</style>
---
+++ pokemon/src/views/Home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <PkmnList :id="20" :offset="0" />
+  </div>
+</template>
+
+<script>
+import PkmnList from "../components/PkmList";
+export default {
+  name: "Home",
+  components: {
+    PkmnList,
+  },
+};
+</script>
